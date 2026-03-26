a4a652adcbcedf0c84448f190ef2e5cd178c05f7
5mil <5mil@users.noreply.github.com>
2026-03-26 00:05:28 -0400

    Update platform_thread_start function signature

--- platform.h
+++ platform.h
@@ -1,11 +1,13 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#include <pthread.h>
 #include <stdint.h>
 
+typedef pthread_t platform_handle_t;
 typedef void *(*platform_thread_fn)(void *);
 
-int      platform_thread_start(void **handle, platform_thread_fn fn, void *arg);
+int      platform_thread_start(platform_handle_t *handle, platform_thread_fn fn, void *arg);
 void     platform_sleep_ms(uint32_t ms);
 uint64_t platform_now_ms(void);
 void     platform_log(const char *fmt, ...);
